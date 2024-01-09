853af39ad9597a94683b97dfa23586b14c896d37
jchristopherson <jchristopherson@hotmail.com>
2024-01-09 14:43:51 -0600

    Initial commit

---
+++ docs.md
@@ -0,0 +1,16 @@
+---
+project: FSTRING
+summary: A Fortran string library that also supports regular expressions.
+project_github: https://github.com/jchristopherson/fstring
+author: Jason Christopherson
+author_email: jchristopherson@hotmail.com
+src_dir: ./src
+output_dir: ./doc
+display: public
+         protected
+source: true
+proc_internals: true
+sort: permission-alpha
+print_creation_date: true
+creation_date: %Y-%m-%d %H:%M %z
+---
---
+++ test/fstring_test.f90
@@ -0,0 +1,19 @@
+program main
+    use string_tests
+    implicit none
+
+    if (.not.test_string_equality()) stop 1
+    if (.not.test_split_string()) stop 2
+    if (.not.test_to_string()) stop 3
+    if (.not.test_to_c_string()) stop 4
+    if (.not.test_string_to_int()) stop 5
+    if (.not.test_string_to_real()) stop 6
+    if (.not.test_string_builder()) stop 7
+    if (.not.test_remove()) stop 8
+    if (.not.test_insert()) stop 9
+    if (.not.test_regex_match()) stop 10
+    if (.not.test_regex_search()) stop 11
+    if (.not.test_replace()) stop 12
+    if (.not.test_to_upper()) stop 13
+    if (.not.test_to_lower()) stop 14
+end program
